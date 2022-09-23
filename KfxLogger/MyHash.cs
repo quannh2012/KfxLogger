aff4df182e8f20f3fb386d9166b2e105328a5e3b
Quan Nguyen <quanhong.nguyen@kofax.com>
2022-09-23 13:56:45 +0700

    Init

---
+++ KfxLogger/MyHash.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace KfxLogger
+{
+    class MyHash
+    {
+        public delegate string CalculateHash(string filename);
+
+        public static string Md5(object o)
+        {
+            var tmpSource = ASCIIEncoding.ASCII.GetBytes(""+o);
+            using (var md5 = MD5.Create())
+            {
+                var hash = md5.ComputeHash(tmpSource);
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    // can be "x2" if you want lowercase
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+        public static string GetMd5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    var sb = new StringBuilder(hash.Length * 2);
+
+                    foreach (byte b in hash)
+                    {
+                        // can be "x2" if you want lowercase
+                        sb.Append(b.ToString("x2"));
+                    }
+
+                    return sb.ToString();
+                }
+            }
+        }
+        public static string GetSha1(string filename)
+        {
+            using (var stream = File.OpenRead(filename))
+            {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    var hash = sha1.ComputeHash(stream);
+                    var sb = new StringBuilder(hash.Length * 2);
+
+                    foreach (byte b in hash)
+                    {
+                        // can be "x2" if you want lowercase
+                        sb.Append(b.ToString("x2"));
+                    }
+
+                    return sb.ToString();
+                }
+            }
+        }
+
+        public static string GetSha256(string filename)
+        {
+            using (var stream = File.OpenRead(filename))
+            {
+                using (var sha = new SHA256Managed())
+                {
+                    var hash = sha.ComputeHash(stream);
+                    var sb = new StringBuilder(hash.Length * 2);
+
+                    foreach (byte b in hash)
+                    {
+                        // can be "x2" if you want lowercase
+                        sb.Append(b.ToString("x2"));
+                    }
+
+                    return sb.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ KfxLogger/TwoProjectCompare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace KfxLogger
+{
+    internal class TwoProjectCompare
+    {
+        internal static void Do()
+        {
+            var prj1 = @"C:\z\Project_KAFTA_09212022 17-40-04-487 STG";
+            var prj2 = @"C:\z\Project_KAFTA_09212022 17-42-59-529 TestCBA";
+            var files1 = Directory.GetFiles(prj1, "*", SearchOption.AllDirectories).ToList();
+            var files2 = Directory.GetFiles(prj2, "*", SearchOption.AllDirectories).ToList();
+
+            var rmr1 = files1.Select(x => x.Replace(prj1, string.Empty)).ToList();
+            var rmr2 = files2.Select(x => x.Replace(prj2, string.Empty)).ToList();
+            var z = rmr1.Except(rmr2).ToList();
+            z = z.Where(x => x.ToLower().Contains("record") || x.ToLower().Contains("metric")).ToList();
+        }
+    }
+}
